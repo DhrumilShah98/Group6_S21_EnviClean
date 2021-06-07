919bb7a95f84e50720a8e4a126c2cdf987ce62e1
DhrumilShah98 <shah.dhrumil1998@gmail.com>
2021-06-07 05:57:19 +0530

    Patron member implemented

--- src/component/PatronMember/PatronMember.js
+++ src/component/PatronMember/PatronMember.js
@@ -1,10 +1,16 @@
 import patronMemberImg from '../../images/patron_member.png';
-import { Card, CardContent, Paper, Typography, CssBaseline, CardActions, Button, Avatar } from '@material-ui/core';
-import React from 'react';
+import { Card, CardContent, Paper, Typography, CssBaseline, Button, Avatar, CardActions } from '@material-ui/core';
+import React, { useState } from 'react';
 import useStyles from './styles';
+import { PatronTermsAndConditions } from './PatronTermsAndConditions';
 
 export const PatronMember = () => {
     const classes = useStyles();
+    const [open, setOpen] = useState(false);
+
+    const openDialog = () => { setOpen(true); }
+
+    const closeDialog = () => { setOpen(false); }
 
     return (
         <div className={classes.center}>
@@ -21,10 +27,13 @@ export const PatronMember = () => {
                         Keeping the environment clean is our motive.
                     </Typography>
                 </CardContent>
-                <Button className={classes.button} style={{ backgroundColor: '#719D57' }} size="large" variant="contained" fullWidth>
-                    Enroll as Patron
-                </Button>
+                <CardActions>
+                    <Button className={classes.button} style={{ backgroundColor: '#719D57' }} onClick={openDialog} size="large" variant="contained" fullWidth>
+                        Enroll as Patron
+                    </Button>
+                </CardActions>
             </Card>
+            <PatronTermsAndConditions open={open} closeDialog={closeDialog} />
         </div>
     )
 }
