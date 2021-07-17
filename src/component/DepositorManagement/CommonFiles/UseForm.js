e77319cfc590646caf63c2dc1ea607fd3b165c64
Sriram Attanti <sr601872@dal.ca>
2021-07-18 02:11:09 +0530

    Added APIs for deposit frontend

--- src/component/DepositorManagement/CommonFiles/UseForm.js
+++ src/component/DepositorManagement/CommonFiles/UseForm.js
@@ -1,10 +1,9 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import Validation from "./Validation";
 import axios from "axios";
 import { Constants } from "../../../config/constants";
 
 function UseForm(edit) {
-
   const [values, setValues] = useState({
     taskname: "",
     drywaste: false,
@@ -17,90 +16,104 @@ function UseForm(edit) {
     address: "",
     pincode: "",
     phonenumber: "",
-    instructions:"",
+    instructions: "",
   });
   const [errormessage, setErrorMessage] = useState();
   const [errors, setErrors] = useState({});
   const [isValid, setIsValid] = useState(false);
   function handleChange(event) {
-    console.log(event.target.name + " " + event.target.value);
-      setValues({
-        ...values,
-        [event.target.name]: event.target.value,
-      });
-      console.log("values: ", values)
-      console.log("edit", edit)
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-
     setErrors(Validation(values));
     setIsValid(true);
-    if (Object.keys(errors).length === 0 && isValid)
-    {
-      let user = JSON.parse(localStorage.getItem("user"));
-        console.log(user);
+    console.log(Object.keys(errors).length)
+    console.log(isValid)
+    if (Object.keys(errors).length === 0 && isValid) {
+      let user = localStorage.getItem("user");
+      console.log(user);
+      if (user) {
+        user = JSON.parse(user);
         const emailId = user.email;
         console.log(emailId);
-      axios
-      .post(Constants.CREATE_DEPOSIT, {
-        userId: 1,
-        taskname: values.taskname,
-        drywaste: values.drywaste,
-        wetwaste: values.wetwaste,
-        medicalwaste: values.medicalwaste,
-        others: values.others,
-        weight: values.weight,
-        schedule: values.schedule,
-        datetime: values.datetime,
-        address: values.address,
-        pincode: values.pincode,
-        phonenumber: values.phonenumber,
-        instructions: values.instructions
-      })
-      .then(() => {
-        window.location.reload();
-      })
-      .catch((error) => {
-        if (error.response != undefined) {
-          setErrorMessage(error.response.data.message);
-        } else {
-          setErrorMessage(
-            "Error in sending request. Please try again later."
-          );
-        }
-      });
-    }
+        axios
+          .post(Constants.CREATE_DEPOSIT, {
+            userId: emailId,
+            taskname: values.taskname,
+            drywaste: values.drywaste,
+            wetwaste: values.wetwaste,
+            medicalwaste: values.medicalwaste,
+            others: values.others,
+            weight: values.weight,
+            schedule: values.schedule,
+            datetime: values.datetime,
+            address: values.address,
+            pincode: values.pincode,
+            phonenumber: values.phonenumber,
+            instructions: values.instructions,
+          })
+          .then(() => {
+            if (Object.keys(errors).length === 0 && isValid) {
+              setValues({
+                taskname: "",
+                drywaste: false,
+                wetwaste: false,
+                medicalwaste: false,
+                others: false,
+                weight: "",
+                schedule: "Once",
+                datetime: "",
+                address: "",
+                pincode: "",
+                phonenumber: "",
+                instructions: "",
+              });
+            }
+            window.location.reload();
+          })
+          .catch((error) => {
+            if (error.response != undefined) {
+              setErrorMessage(error.response.data.message);
+            } else {
+              setErrorMessage(
+                "Error in sending request. Please try again later."
+              );
+            }
+          });
+      }
+      else {
+        setValues({
+          taskname: "",
+          drywaste: false,
+          wetwaste: false,
+          medicalwaste: false,
+          others: false,
+          weight: "",
+          schedule: "Once",
+          datetime: "",
+          address: "",
+          pincode: "",
+          phonenumber: "",
+          instructions: "",
+        });
+        return window.alert("Please Login to Create Deposit");
+      }
+    } 
   }
 
   useEffect(() => {
-      if (Object.keys(errors).length === 0 && isValid) {
-       setValues({
-        taskname: "",
-        drywaste: false,
-        wetwaste: false,
-        medicalwaste: false,
-        others: false,
-        weight: "",
-        schedule: "Once",
-        datetime: "",
-        address: "",
-        pincode: "",
-        phonenumber: "",
-        instructions:"",
+    if (edit != null) {
+      setValues({
+        ...edit,
       });
-      }
-     if (edit != null)
-      {
-        setValues({
-          ...edit
-        });
-      console.log("values 2", values)
-      console.log("Edit 2 ", edit)
 
     }
-     //eslint-disable-next-line
+    //eslint-disable-next-line
   }, [errors]);
   return { handleChange, handleSubmit, values, errors, errormessage };
 }
--- src/component/DepositorManagement/ViewDeposits/ViewDeposits.js
+++ src/component/DepositorManagement/ViewDeposits/ViewDeposits.js
@@ -1,11 +1,10 @@
-import dummyViewData from "./DummyDepositData";
 import { Card, CardContent, Typography, Grid, IconButton } from "@material-ui/core";
 import useStyles from "./styles.js";
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import ModifyDeposit from "../ModifyDeposit/ModifyDeposit";
 import DeleteDeposit from "../DeleteDeposit/DeleteDeposit";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PopUp from "../CommonFiles/PopUp";
 import axios from "axios";
 import { Constants } from "../../../config/constants";
@@ -17,24 +16,54 @@ function ViewDeposits() {
   const [id, setId]= useState("");
   const [openDelete, setopenDelete] =useState(false);
   const [edit, setEdit]= useState("null");
+  const [depositdata, setDepositData] = useState([]);
 
   let user = localStorage.getItem("user");
-  let depositdata = []
-  if (user) {
+  
+  useEffect(() => {
+    if (user) {
       user = JSON.parse(user);
       let emailId = user.email;
       if (emailId) {
           axios.get(Constants.VIEW_DEPOSIT+ emailId).then(
               (response) => {
-                  depositdata = response.data.payload;                  
+                  setDepositData( response.data.payload)
               });
       }
   }
+    //eslint-disable-next-line
+  }, []);
 
   function handleModify(data)
   {
-    console.log("entered" + data.id)
-    console.log(data)
+    if (data.drywaste === 1)
+    {
+      data.drywaste = true;
+    }
+    else {
+      data.drywaste = false;
+    }
+    if (data.wetwaste === 1)
+    {
+      data.wetwaste = true;
+    }
+    else {
+      data.wetwaste = false;
+    }
+    if (data.medicalwaste === 1)
+    {
+      data.medicalwaste = true;
+    }
+    else {
+      data.medicalwaste = false;
+    }
+    if (data.others === 1)
+    {
+      data.others = true;
+    }
+    else {
+      data.others = false;
+    }
     setEdit(data)
     setopenEdit(true)
   }
@@ -44,7 +73,7 @@ function ViewDeposits() {
     setopenDelete(true)
   }
   
-  if (!depositdata.length){
+  if (depositdata.length === 0){
     return (
       <div style={{ paddingTop: 50 }}>
       <Typography variant="h6" style={{ color: "#154001", fontWeight: 600 }}>My Deposits</Typography>
@@ -74,24 +103,24 @@ function ViewDeposits() {
       >
         {depositdata.map((data) => {
           let garbagetype = "";
-          if (data.drywaste)
+          if (data.drywaste === 1)
           {
             garbagetype += " " + "drywaste";
           }
-          if (data.wetwaste)
+          if (data.wetwaste === 1)
           {
             garbagetype += " " + "wetwaste";
           }
-          if (data.medicalwaste)
+          if (data.medicalwaste === 1)
           {
             garbagetype += " " + "medicalwaste";
           }
-          if (data.others)
+          if (data.others === 1)
           {
             garbagetype += " " + "others";
           }
             return (
-              <Grid key={data._id} item xs={12} sm={6} md={5}>
+              <Grid key={data.id} item xs={12} sm={6} md={5}>
                 <Card className={classes.card} elevation={6}>
                   <CardContent>
                     <Typography
@@ -130,7 +159,7 @@ function ViewDeposits() {
                       </EditIcon>
                     </IconButton>
                     <IconButton
-                      variant="contained"  onClick = {()=>handleDelete(data._id)}>
+                      variant="contained"  onClick = {()=>handleDelete(data.id)}>
                         <DeleteIcon style={{ color: '#F44336' }}>
                       Delete
                       </DeleteIcon>
