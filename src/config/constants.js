472f1beaecf9a284579a1f81f8948c9df38d6b3e
vishaldipakparmar <vs301167@dal.ca>
2021-07-18 17:52:37 -0300

    Change URLs in constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,23 +1,23 @@
 export class Constants {
 
     /* User Managment APIS Start */
-    static USER_LOGIN = "http://localhost:3001/app/login";
-    static USER_SIGNUP = "http://localhost:3001/app/signup";
-    static USER_FORGOT_PASSWORD = "http://localhost:3001/app/forgotpassword";
-    static USER_GET = "http://localhost:3001/user/get/";
-    static USER_UPDATE = "http://localhost:3001/user/update/";
-    static USER_DELETE = "http://localhost:3001/user/delete/";
+    static USER_LOGIN = "https://envicleanapi.herokuapp.com/app/login";
+    static USER_SIGNUP = "https://envicleanapi.herokuapp.com/app/signup";
+    static USER_FORGOT_PASSWORD = "https://envicleanapi.herokuapp.com/app/forgotpassword";
+    static USER_GET = "https://envicleanapi.herokuapp.com/user/get/";
+    static USER_UPDATE = "https://envicleanapi.herokuapp.com/user/update/";
+    static USER_DELETE = "https://envicleanapi.herokuapp.com/user/delete/";
     /* User Managment APIS End */
 
     // Notification settings API start
-    static GET_NOTIFICATION_SETTINGS = "http://localhost:3001/settigs/notifications/";
-    static MODIFY_NOTIFICATION_SETTINGS = "http://localhost:3001/settings/notifications/";
+    static GET_NOTIFICATION_SETTINGS = "https://envicleanapi.herokuapp.com/settigs/notifications/";
+    static MODIFY_NOTIFICATION_SETTINGS = "https://envicleanapi.herokuapp.com/settings/notifications/";
     // Notification settings APIS End
 
     /* Depositor APIs Start*/
-    static CREATE_DEPOSIT = "http://localhost:3001/depositor/createdeposit";
-    static VIEW_DEPOSIT = "http://localhost:3001/depositor/getdeposits/";
-    static UPDATE_DEPOSIT = "http://localhost:3001/depositor/updatedeposit/";
-    static DELETE_DEPOSIT = "http://localhost:3001/depositor/deletedeposit/";
+    static CREATE_DEPOSIT = "https://envicleanapi.herokuapp.com/depositor/createdeposit";
+    static VIEW_DEPOSIT = "https://envicleanapi.herokuapp.com/depositor/getdeposits/";
+    static UPDATE_DEPOSIT = "https://envicleanapi.herokuapp.com/depositor/updatedeposit/";
+    static DELETE_DEPOSIT = "https://envicleanapi.herokuapp.com/depositor/deletedeposit/";
     /* Depositor APIs END*/
 }
